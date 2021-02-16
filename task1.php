eba65f0fa2c685085eb09753065ff60e4ac45411
densbv <63791038+densbv@users.noreply.github.com>
2021-02-16 18:19:17 +0700

    Initial commit

---
+++ task1.php
@@ -0,0 +1,27 @@
+<?php
+
+class SimpleClass {
+
+    private $array = [];
+    
+    public function __invoke()
+    {
+        return $this;
+    }
+
+    public function stdout($str) 
+    {
+        $this->array[] = $str;
+        $newStr = implode(' ', $this->array);
+
+        echo '<script>console.log("' . $newStr . '")</script>';
+
+        return $this;
+    }
+
+}
+
+$obj = new SimpleClass;
+$obj()->stdout('Hello')->stdout('World');
+
+
