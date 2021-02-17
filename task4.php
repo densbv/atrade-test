e11b7474b7b253734e87b27fe39d75fb5d0f41d2
densbv <63791038+densbv@users.noreply.github.com>
2021-02-17 23:43:28 +0800

    Update task4.php

--- task4.php
+++ task4.php
@@ -1,26 +1,25 @@
 <?php
+function getNum($max)
+{
+    while(true){
+        $cur = 1;
+        $prev = 0;
 
-function getNum($max) {
-    $f = [];
-    for ($i = 1; $i < $max + 1; $i++) {
-        if ($i === 1) {
-            $f[$i] = $i;
-        }
-        if ($i > 1) {
-            $f[$i] = $f[$i - 1] + $f[$i - 2];
+        for ($i = 0; $i < $max; $i++) {
+            yield $cur;
+
+            $temp = $cur;
+            $cur = $prev + $cur;
+            $prev = $temp;
         }
     }
-    return $obj = (object) $f;
-    //var_dump($f);
+    
 }
 
-//$data = getNum(5);
-//
-//for ($i = 0; $i < 10; ++$i) {
-//
-//    print $data->current() . PHP_EOL;
-//
-//    $data->next();
-//}
-//var_dump(getNum(10));
-foreach(getNum(6) as $val) print $val.PHP_EOL; 
+$data = getNum(5);
+for ($i = 0; $i < 10; ++$i) { 
+    print nl2br($data->current() . PHP_EOL);
+    $data->next(); 
+}
+
+foreach(getNum(5) as $val) print $val.PHP_EOL; 
