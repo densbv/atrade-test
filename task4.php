9796701eeefb77f3bcb466a06803c78abb20b8bc
densbv <63791038+densbv@users.noreply.github.com>
2021-02-17 17:51:19 +0700

    Update task4.php

--- task4.php
+++ task4.php
@@ -1,25 +1,26 @@
 <?php
 
-function fib($line) {
+function getNum($max) {
     $f = [];
-    for ($i = 0; $i < $line; $i++) {
-        if ($i <= 1) {
+    for ($i = 1; $i < $max + 1; $i++) {
+        if ($i === 1) {
             $f[$i] = $i;
-            //echo $f[$i] . ' ';
         }
         if ($i > 1) {
             $f[$i] = $f[$i - 1] + $f[$i - 2];
-            //echo $f[$i] . ' ';
         }
     }
     return $obj = (object) $f;
+    //var_dump($f);
 }
 
-$data = getNum(5);
-
-for ($i = 0; $i < 10; ++$i) {
-
-    print $data->current() . PHP_EOL;
-
-    $data->next();
-}
+//$data = getNum(5);
+//
+//for ($i = 0; $i < 10; ++$i) {
+//
+//    print $data->current() . PHP_EOL;
+//
+//    $data->next();
+//}
+//var_dump(getNum(10));
+foreach(getNum(6) as $val) print $val.PHP_EOL; 
