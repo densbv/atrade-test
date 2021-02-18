6e8c16bc07a507cf64e69b2f215afea0a2013b9f
densbv <63791038+densbv@users.noreply.github.com>
2021-02-18 12:46:56 +0700

    Update task5.php

--- task5.php
+++ task5.php
@@ -11,10 +11,4 @@ $date = date("d.m.Y");
 $trans = ['{DATE}' => $date, '{TIMESTAMP}' => $time];
 $text = strtr($text, $trans);
 
-//$patterns = ['/{DATE}/', '/{TIMESTAMP}/'];
-//$replacements = [$date, $time];
-//
-//$text = preg_replace($patterns, $replacements, $text);
-
-
 print $text;
